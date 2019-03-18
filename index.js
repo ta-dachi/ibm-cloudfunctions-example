6e865ad8b57a1eeeb5a29ad22af57471b06b2cf7
Takumi Adachi <takumiadachi@gmail.com>
2019-03-18 14:07:45 -0700

    Added some stuff with dummy ibm cloud functions.

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 import axios from "axios";
 
+async function publicGET(url) {
+  try {
+    const data = await axios.get(url);
+    return data;
+  } catch (error) {
+    return error;
+  }
+}
+
+async function test() {
+  const data = await axios.get(
+    "https://us-south.functions.cloud.ibm.com/api/v1/web/takumiadachi%40gmail.com_dev/get-http-resource/weather.json"
+  );
+  return data;
+}
+
 function main() {
   console.log("test");
   axios
@@ -10,18 +26,15 @@ function main() {
       console.log(response.data);
     });
 
-  async function test() {
-    const data = await axios.get(
-      "https://us-south.functions.cloud.ibm.com/api/v1/web/takumiadachi%40gmail.com_dev/get-http-resource/weather.json"
-    );
-    return data;
-  }
+  test().then(response => {
+    console.log(response.data);
+  });
 
-  console.log(
-    test().then(response => {
-      console.log(response.data);
-    })
-  );
+  publicGET(
+    "https://us-south.functions.cloud.ibm.com/api/v1/web/takumiadachi%40gmail.com_dev/ibm-cloudfunctions-example/Hello.json"
+  ).then(response => {
+    console.log(response.data);
+  });
 }
 
 main();
